2713e575e8189336fced32e3b7ddde9844d33c60
tomtom815 <98099798+tomtom815@users.noreply.github.com>
2022-02-26 10:19:53 -0600

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -1,10 +1,10 @@
 <?php
     class Database{
         // DB Params
-        private $host = "localhost";
-        private $db_name = "myblog";
-        private $username = "root";
-        private $password = "";
+        private $host = "i0rgccmrx3at3wv3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        private $db_name = "xie5f21y231r8u9o";
+        private $username = "lj7mjlq2t2qvzdzh";
+        private $password = "ascwyhqzhb0nn7gx";
         private $conn;
 
 
